a702fc5af79be9d8ab3be2399db08dcbf0b8a4b3
Muhamad Farhan <126461417+mrglxor@users.noreply.github.com>
2023-12-13 18:55:51 +0700

    add a cors in rule

--- src/main.js
+++ src/main.js
@@ -8,6 +8,3 @@ const { PORT } = process.env;
 web.listen(PORT, () => {
     logger.info(`App start in http://localhost:${PORT}`);
 });
-
-web.get("/", (req, res) => res.send("Welcome to the Management Contact API!"));
-web.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
