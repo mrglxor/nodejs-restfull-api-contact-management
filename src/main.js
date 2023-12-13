ce4ec7ab82445c2a59f218d61aa0eab18dfc7d7f
Muhamad Farhan <126461417+mrglxor@users.noreply.github.com>
2023-12-13 10:51:52 +0700

    fixing

--- src/main.js
+++ src/main.js
@@ -8,3 +8,6 @@ const { PORT } = process.env;
 web.listen(PORT, () => {
     logger.info(`App start in http://localhost:${PORT}`);
 });
+
+web.get("/", (req, res) => res.send("Welcome to the Management Contact API!"));
+web.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
